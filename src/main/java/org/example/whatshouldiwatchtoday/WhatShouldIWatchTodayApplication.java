7da30c4d30bc12bf187c4d370e4ddcbc1fb23f57
junggkim <kjk1526@naver.com>
2025-05-14 11:37:01 +0900

    main

---
+++ src/main/java/org/example/whatshouldiwatchtoday/WhatShouldIWatchTodayApplication.java
@@ -0,0 +1,13 @@
+package org.example.whatshouldiwatchtoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatShouldIWatchTodayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatShouldIWatchTodayApplication.class, args);
+    }
+
+}
